e1a91a12b0a0587a0604e910ddaf2a9ae7ae823e
Rizky Luthfianto <mrluthfianto@gmail.com>
2015-11-11 09:14:45 +0700

    gabung ga ya

--- app/src/main/java/com/bananastech/nitip/activities/seller/SellerMainActivity.java
+++ app/src/main/java/com/bananastech/nitip/activities/seller/SellerMainActivity.java
@@ -10,6 +10,7 @@ import com.bananastech.nitip.holders.TripViewHolder;
 import com.bananastech.nitip.models.TripModel;
 import com.skocken.efficientadapter.lib.adapter.EfficientRecyclerAdapter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import android.support.v7.widget.Toolbar;
 import butterknife.Bind;
@@ -36,8 +37,14 @@ public class SellerMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_seller);
         ButterKnife.bind(this);
+
         List<TripModel> tripModelList = new ArrayList<TripModel>();
-        tripModelList.add(new TripModel("Jakarta", "Johannesburg", 0, 0));
+        long now = new Date().getTime();
+        tripModelList.add(new TripModel("Jakarta", "Johannesburg", now, now+100000000));
+        tripModelList.add(new TripModel("Jakarta", "Singapore", now+200000000, now+1000000000));
+        tripModelList.add(new TripModel("Bandung", "Manado", now+1500000000, now+2100000000));
+        tripModelList.add(new TripModel("Bandung", "Balikpapan", now+(long) 2400000000.0, now+ (long) 2800000000.0));
+
         EfficientRecyclerAdapter<TripModel> adapter = new EfficientRecyclerAdapter<TripModel>(R.layout.row_trip, TripViewHolder.class, tripModelList);
         recyclerViewTrips.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewTrips.setAdapter(adapter);
